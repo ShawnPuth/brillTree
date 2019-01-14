92463d64f869bcd1dd38f3be99e50c0448168a36
946818508@qq.com <scl199303>
2019-01-14 17:35:41 +0800

    *

--- src/Controller/AdjacencyList.php
+++ src/Controller/AdjacencyList.php
@@ -25,7 +25,7 @@ EOF;
         $js = file_get_contents(__DIR__.'/../Static/dendrogram.js');
 
         $data = $array = [
-     ["id"=>1,"p_id"=>2,"name"=>"中国"],
+     ["id"=>1,"p_id"=>0,"name"=>"中国"],
      ["id"=>2,"p_id"=>1,"name"=>"四川"],
      ["id"=>3,"p_id"=>1,"name"=>"北京"],
      ["id"=>4,"p_id"=>2,"name"=>"成都"]];
